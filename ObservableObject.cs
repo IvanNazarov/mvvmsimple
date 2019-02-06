2bb0f01fbab6d76be873081aa1818cc056399258
Ivan Nazarov <ivan.n2331@gmail.com>
2019-02-06 22:18:26 +0300

    Base MVVM classes added to project

---
+++ ObservableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace mvvmsimple
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ViewModelBase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+using Windows.ApplicationModel;
+
+namespace mvvmsimple
+{
+    public class ViewModelBase : ObservableObject
+    {
+        private static bool? _isInDesignMode;
+
+        public static bool IsInDesignModeStatic
+        {
+            get
+            {
+                if (!_isInDesignMode.HasValue)
+                {
+                    _isInDesignMode = DesignMode.DesignModeEnabled;
+                }
+                return _isInDesignMode.Value;
+            }
+        }
+
+        protected bool Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+                return false;
+            field = newValue;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+
+    }
+}
